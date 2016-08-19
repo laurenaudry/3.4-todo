954effb4a93d9dcf4f9187ff8f023e2d39caaf3a
Lauren Johnson <laurenaudry1995@gmail.com>
2016-08-19 14:41:33 -0500

    working on forms

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,13 +1,30 @@
 // Main javascript/jQuery
 $(function() {
+  var $formElement = $("[data-js=todo__form]")
   var $submitElement = $("[data-js='todo__submit']");
   var $textElement = $("[data-js='todo__text']");
   var $listElement = $("[data-js='todo__items']");
-
-  // prevents default on submit
-  $submitElement.on("return", function(e){
-      e.preventDefault();
-    });
+  var selectedTodo = [];
 
 
+    $submitElement.on("click", function(e){
+    // prevents default submit behavior
+    e.preventDefault();
+
+    var $listString = $textElement.val();
+
+    if($listString.length >= 2){
+      $listElement.append(`
+        <li class="toDo__listItem">
+          <span class="checkBox"></span>
+          <p  data-js="toDo__listText"
+              class="toDo__listText">
+            ${$listString}
+          </p>
+        </li>
+        `);
+
+        $textElement.val("");
+      };
+  });
 });
